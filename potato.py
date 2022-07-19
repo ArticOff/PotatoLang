180da32c6df9f83dabfff237681dd297170249b1
Artic <81034458+ArticOff@users.noreply.github.com>
2022-07-19 19:00:13 +0200

    Update potato.py

--- potato.py
+++ potato.py
@@ -1,13 +1,12 @@
-import sys
+import sys 
 
 __variables__ = {}
 
 def _print(line: str, contents: str):
     print_statement = line.split("\n")[0].replace("print", "").replace(";", "").replace("(", "").replace(")", "")
-    if print_statement[0] == '"' or print_statement[0] == "'":
-        print_statement = print_statement[1:]
     if print_statement[-1] == '"' or print_statement[-1] == "'":
         print_statement = print_statement[:-1]
+        print_statement = print_statement[1:]
         print(print_statement)
     else:
         for variable in __variables__:
@@ -26,44 +25,63 @@ def _id(line: str, contents: str):
             line_number = contents.split("\n").index(line)
             name = line.split("\n")[0].split("(")[0]
             print(f"NameError: The name {name} is not defined in line {line_number + 1}")
-            return sys.exit(1)
+            return sys.exit(1) 
         else:
             line_number = contents.split("\n").index(line)
             return print(f"SyntaxError: Missing dollar sign in line {line_number + 1}")
 
 def _condition(line: str, contents: str):
+    def line_after_if():
+        line_number = contents.split("\n").index(line)
+        return contents.split("\n")[line_number + 1]
     if "if" in line:
         if "(" in line:
             if ")" in line:
                 value = line.split("\n")[0].split("(")[1].split(")")[0]
-                if value == True:
-                    # get all lines with a tab in front of them and after the if
-                    lines = []
-                    for line in contents.split("\n"):
-                        if line.startswith("\t"):
-                            lines.append(line)
-                    for line in lines:
-                        if "else" in line:
-                            return
-                        print('a')
+                if "==" in value:
+                    if value.split("==")[0] in __variables__:
+                        if value.split("==")[1] in __variables__:
+                            if __variables__[value.split("==")[0]] == __variables__[value.split("==")[1]]:
+                                _print(line_after_if(), contents)
+                            else:
+                                return False
+                        else:
+                            if __variables__[value.split("==")[0]] == value.split("==")[1]:
+                                _print(line_after_if(), contents)
+                            else:
+                                return False
+                    else:
+                        if value.split("==")[1] in __variables__:
+                            if value.split("==")[0] == __variables__[value.split("==")[1]]:
+                                _print(line_after_if(), contents)
+                            else:
+                                return False
+                        else:
+                            if __variables__[value.split("==")[0]] == value.split("==")[1]:
+                                _print(line_after_if(), contents)
+                            else:
+                                return False
                 else:
-                    return False
+                    line_number = contents.split("\n").index(line)
+                    print(f"SyntaxError: missing == in line {line_number + 1}")
+                    sys.exit(1)
             else:
                 line_number = contents.split("\n").index(line)
                 print(f"SyntaxError: Unbalanced parentheses in line {line_number + 1}")
                 sys.exit(1)
 
-
 file = sys.argv[1]
-with open(file, "r") as f:
+with open(file, "r", encoding="utf-8") as f:
     contents = f.read()
     for line in contents.split("\n")[:]:
         if ";" in line:
             if "=" in line:
-                    variables = line.split("\n")[0]
-                    var_value = variables[variables.index("=") + 1 :].removesuffix(";").replace(" ", "", 1).replace('"', "")
-                    var_name = variables[:variables.index("=")].removesuffix(";").replace(" ", "")
-                    __variables__[var_name] = var_value
+                if not "(" in line:
+                    if not ")" in line:
+                        variables = line.split("\n")[0]
+                        var_value = variables[variables.index("=") + 1 :].removesuffix(";").replace(" ", "", 1).replace('"', "")
+                        var_name = variables[:variables.index("=")].removesuffix(";").replace(" ", "")
+                        __variables__[var_name] = var_value
             elif "print" in line:
                 if line.count("\"") % 2 == 0:
                     _print(line, contents)
@@ -98,10 +116,15 @@ with open(file, "r") as f:
                     print(f"NameError: The name {name} is not defined in line {line_number + 1}")
                     sys.exit(1)
         elif ":" in line:
-            _condition(line, contents)
+            if "#" in line:
+                continue
+            else:
+                _condition(line, contents)
                 
         else:
-            if line.startswith("#"):
+            # check if the line contains a # 
+            if "#" in line:
+                # if it does, remove the comment
                 continue
             elif line == "":
                 continue
