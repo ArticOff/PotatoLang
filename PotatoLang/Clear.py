7fa110a857b5be6a6b7ab82e5bfe5ada6ff96efc
Artic <artic.admisoffi@gmail.com>
2022-10-08 22:30:56 +0200

    Delete PotatoLang directory

--- PotatoLang/Clear.py
+++
@@ -1,4 +0,0 @@
-def GarbageCollection(_global: dict) -> None:
-    for variable in list(_global):
-        if str(variable).startswith("TEMP_"):
-            del _global[variable]
--- PotatoLang/Constants.py
+++
@@ -1,2 +0,0 @@
-__Keywords__ = ["is", "and", "or", "in", "for", "build", "declare", "if", "not", "with", "as"]
-__Version__ = "v2.2.0rc1"
